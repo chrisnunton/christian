c688a2918bf21f6c9ae951f6f08ef7643066125e
chrisnunton <chrisjaccyn_10@hotmail.com>
2018-09-30 13:00:35 -0500

    commit

---
+++ app/src/main/java/com/example/chris/oaoaoaoa/PantallaInicioActivity.java
@@ -0,0 +1,14 @@
+package com.example.chris.oaoaoaoa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PantallaInicioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_inicio);
+    }
+
+}
