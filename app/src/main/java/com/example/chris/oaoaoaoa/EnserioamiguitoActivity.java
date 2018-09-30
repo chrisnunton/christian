82491f13ff0899c3c5f27612f4cf1ab0a1d6fdfc
chrisnunton <chrisjaccyn_10@hotmail.com>
2018-09-30 13:09:10 -0500

    commit

---
+++ app/src/main/java/com/example/chris/oaoaoaoa/EnserioamiguitoActivity.java
@@ -0,0 +1,13 @@
+package com.example.chris.oaoaoaoa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnserioamiguitoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enserioamiguito);
+    }
+}
